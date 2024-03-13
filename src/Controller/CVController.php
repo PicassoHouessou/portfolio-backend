06efa782f7c97964ad35f2b4bfd836afc1ba301e
Picasso Houessou <houessoupicasso@yahoo.fr>
2024-03-13 11:09:40 +0100

    Remove comment in CV controller

--- src/Controller/CVController.php
+++ src/Controller/CVController.php
@@ -4,35 +4,26 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\File\File;
-use Symfony\Component\HttpFoundation\FileBag;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CVController extends AbstractController
 {
-
-    /**
-     * @Route("/cv/download")
-     * @return BinaryFileResponse
-     */
-    public function cv($projectDirectory, Request $request)
+    #[Route("/cv/download")]
+    public function cv($projectDirectory, Request $request): BinaryFileResponse
     {
-        //$file = new File($this->get('kernel')->getProjectDir().'/public/cvPicassoHouessou.pdf')  ;
-        $lang = $request->get('lang','fr');
+        $lang = $request->get('lang', 'fr');
         $file = "";
-        if ($lang== "fr") {
+        if ($lang == "fr") {
             $file = new File($projectDirectory . '/public/cvPicassoHouessou-fr.pdf');
-        } else{
+        } else {
 
             $file = new File($projectDirectory . '/public/cvPicassoHouessou-en.pdf');
 
         }
-        // rename the downloaded file
         return $this->file($file, 'CV-Picasso-Houessou.pdf');
-
-        //return new BinaryFileResponse($file);
     }
 }
